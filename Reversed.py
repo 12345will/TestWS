ea0b91dde1b662efc538b3ab3927185e33630ae5
12345will <121185031+12345will@users.noreply.github.com>
2025-07-09 14:36:29 +0100

    Update Reversed.py

--- Reversed.py
+++ Reversed.py
@@ -3,6 +3,13 @@ import requests
 from textblob import TextBlob
 import pandas as pd
 import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 import setup
 
 
